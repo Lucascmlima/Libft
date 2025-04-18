9c83ab01aff2c2c49ffee6323df9f7a644ad1431
Lucas Carvalho <lcarvalh@student.42lisboa.com>
2025-04-18 11:32:16 +0100

    tha

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,21 +6,29 @@
 /*   By: lcarvalh <lcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:59:49 by lcarvalh          #+#    #+#             */
-/*   Updated: 2025/04/16 16:22:23 by lcarvalh         ###   ########.fr       */
+/*   Updated: 2025/04/17 11:15:38 by lcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char		*ft_itoa(int n);
+
+static int	nsize(int n);
+
+static char	*cstr(char *str, long nb, long len, long sign);
 
 static int	nsize(int n)
 {
 	int	i;
 
 	i = 0;
-	if (n < 0>)
+	if (n == 0)
+		return (1);
+	if (n < 0)
 	{
 		i++;
-		n *= -n;
+		n = -n;
 	}
 	while (n > 9)
 	{
@@ -29,9 +37,10 @@ static int	nsize(int n)
 	}
 	return (i);
 }
-char	cstr(char *str, long nb, long len,long sign)
+
+static char	*cstr(char *str, long nb, long len, long sign)
 {
-	str[len] = '\0'
+	str[len] = '\0';
 	while (len > 0)
 	{
 		str[len--] = nb % 10 + '0';
@@ -41,28 +50,29 @@ char	cstr(char *str, long nb, long len,long sign)
 		str[len++] = '-';
 	return (str);
 }
+
 char	*ft_itoa(int n)
 {
 	char	*str;
 	long	len;
 	long	nb;
 	long	sign;
-	
+
 	nb = n;
 	sign = 1;
 	len = (long)nsize(nb);
 	if (nb == 0)
 		return (ft_strdup("0"));
 	if (nb == -2147483648)
-		return (ft_strdup("-2147483648"))
+		return (ft_strdup("-2147483648"));
 	if (nb < 0)
 	{
-		nb = -nb
+		nb = -nb;
 		sign = -sign;
 	}
-	str = (char *)malloc(sizeof(char) * (len + 1))
-	if(!str)
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
 		return (NULL);
-	str = cstr(str, nb, len, sign)
-	return (str);	
+	str = cstr(str, nb, len, sign);
+	return (str);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lcarvalh <lcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:29:14 by lcarvalh          #+#    #+#             */
-/*   Updated: 2025/04/15 16:35:15 by lcarvalh         ###   ########.fr       */
+/*   Updated: 2025/04/17 11:00:47 by lcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,45 @@
 # include <stdio.h>
 # include <stdint.h> 
 
+
 // 📏 Funções de checagem de caracteres
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
 int		ft_isalnum(int c);
+int		ft_isalpha(int c);
 int		ft_isascii(int c);
+int		ft_isdigit(int c);
 int		ft_isprint(int c);
 
 // 🧮 Funções de manipulação de números
-int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 // 🔤 Funções de manipulação de strings
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *str, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char	*ft_strdup(const char *s1);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
 // 🧩 Funções de manipulação de memória
-void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
 
 // 🧮 Funções adicionais
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_itoa(int n);
 
 // 🎯 Funções bônus — Linked List
 /*typedef struct s_list
