d07ffa0802e3701841ec1242919810477944522e
Lucas Carvalho Miranda Lima <lcarvalh@c2r6s12.42lisboa.com>
2025-04-20 22:26:47 +0100

    bbdomingo

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -1,24 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcarvalh <lcarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/20 20:14:00 by lcarvalh          #+#    #+#             */
-/*   Updated: 2025/04/20 22:19:00 by lcarvalh         ###   ########.fr       */
+/*   Created: 2025/04/20 20:35:34 by lcarvalh          #+#    #+#             */
+/*   Updated: 2025/04/20 21:00:53 by lcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	if (!lst || !f)
-		return ;
-	while (lst)
-	{
-		f(lst->content);
+	if (!lst || !f || !del)
+		return (NULL);
+	t_list	*newl;
+	t_list	*temp;
+
+	temp = NULL;
+	newl = NULL;
+	while(lst)
+	{	
+		temp = ft_lstnew(lst->content);
+		if (!temp)
+		{
+			ft_lstclear(&newl, del);
+			return (NULL);
+		}
+		temp->content = f(lst->content);
+		ft_lstadd_back(&newl, temp);
 		lst = lst->next;
 	}
+	ft_lstclear(temp, del);
+	return (newl);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lcarvalh <lcarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:29:14 by lcarvalh          #+#    #+#             */
-/*   Updated: 2025/04/19 19:25:13 by lcarvalh         ###   ########.fr       */
+/*   Updated: 2025/04/20 20:32:20 by lcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
-//void	ft_lstiter(t_list *lst, void (*f)(void *));
+void 	ft_lstiter(t_list *lst, void (*f)(void *));
 //t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
